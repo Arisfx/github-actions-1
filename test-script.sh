c80516efac72d65e1322d67dd758fab74d54f4df
arisfx <karaiskos.aris@gmail.com>
2024-04-08 14:57:42 +0200

    checking out repo with script file

---
+++ test-script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Running a test script against the container"
+
+# Here you could run a set of tests or interact with the container
+# For demonstration purposes, we will simply echo a message
+echo "Test completed successfully."
